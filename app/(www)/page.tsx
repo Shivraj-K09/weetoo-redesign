e7fb65b76610410e32734bf412e1bb9f47a6b387
Shivraj <sdkadgond9+github@gmail.com>
2025-06-04 15:15:05 +0530

    fix: fixed build issue

--- app/(www)/page.tsx
+++ app/(www)/page.tsx
@@ -1,6 +1,3 @@
-import { ArrowRight } from "lucide-react";
-import Link from "next/link";
-
 export default function Home() {
   return <div className="flex-1 flex items-center justify-center">sss</div>;
 }

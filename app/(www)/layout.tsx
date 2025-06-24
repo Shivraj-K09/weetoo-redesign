c760f0e9edf42e374a0bfb0b88ccf0b1a34be3a8
Shivraj <sdkadgond9+github@gmail.com>
2025-06-24 10:14:36 +0530

    add: added metadata to layout

--- app/(www)/layout.tsx
+++ app/(www)/layout.tsx
@@ -1,6 +1,13 @@
 import { FloatingChat } from "@/components/chat/floating-chat";
 import { Footer } from "@/components/footer";
 import { Header } from "@/components/header";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Weetoo - We Trade",
+  description:
+    "Weetoo is a platform for the trading simulation of cryptocurrencies, stocks, and forex. Join us to learn, practice, and compete in a risk-free environment.",
+};
 
 export default function RootLayout({
   children,
